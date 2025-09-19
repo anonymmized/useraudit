81c260d48e5bea5a5a4ef33cc99633b73d018b34
anonymmized <lih0deev@inbox.ru>
2025-09-19 22:44:53 +0300

    password lenght check were added

--- audit.sh
+++ audit.sh
@@ -10,7 +10,7 @@ fi
 # Create users
 
 create_macos_user() {
-    local NEW_USER FULL_NAME PASS
+    local NEW_USER FULL_NAME PASS PASS2
     read -r -p "Enter username: " NEW_USER
     read -r -p "Enter full name: " FULL
 
@@ -18,8 +18,22 @@ create_macos_user() {
         echo "User '$NEW_USER' already exists." >&2
         return 1
     fi
-
-    read -r -s -p "Enter password for new user: " PASS; echo
+    
+    flag=0
+    while [[ $flag -eq 0 ]]; do
+        read -r -s -p "Enter password for new user: " PASS; echo
+        if [[ ${#PASS} -lt 8 ]]; then 
+            echo "Password too short, enter again"
+        else 
+            read -r -s -p "Enter password again: " PASS2; echo
+            if [[ "$PASS" -eq "$PASS2" ]]; then
+                flag=1
+            else 
+                echo "Incorrect password. Try again"
+            fi
+            
+        fi
+    done
 
     sysadminctl -addUser "$NEW_USER" -fullName "$FULL_NAME" -home "/Users/$USER" -shell "/bin/zsh" -password "$PASS"
     createhomedir -c -u "$NEW_USER" >/dev/null
@@ -31,13 +45,12 @@ create_macos_user() {
 
     if id "$NEW_USER" &>/dev/null; then
         echo "New user $NEW_USER at /Users/$NEW_USER were created"
+        return 0
     else
         echo "Something went wrong"
         exit 1
     fi
-}   
-
-
+}
 
 if [[ "$OS" == "Darwin" ]]; then
     create_macos_user
