c81f34b37464f15c281d152eaa026cd9b81de986
anonymmized <lih0deev@inbox.ru>
2025-10-04 09:31:15 +0300

    remove create user on linux, moved to mac function

--- useraudit.sh
+++ useraudit.sh
@@ -236,107 +236,102 @@ add_to_group() {
     
 }
 
-# ---------- create_linux_user ----------
+# ---------- create_user ----------
 
-create_linux_user() {
+create_user() {
     local FULL_NAME PASS PASS2
     read -r -p "Enter full name: " FULL_NAME
-
+    
     local flag=0
-    while [[ $flag -eq 0 ]]; do
-        read -r -s -p "Enter password for new user: " PASS; printf '\n' >&2
-        if [[ ${#PASS} -lt 8 ]]; then
-            echo "Password too short"
-            read -e -p "Are you sure you want to continue with a short password? [Y/n]: " ANSWY; printf '\n' >&2
-            case "$ANSWY" in 
-                Y|y|Yes|YES|'') ;;
-                N|n|No|NO) continue ;;
-                *) echo "Invalid input. Please enter Y/y or N/n"; continue ;;
-            esac
+    if [[ "$OS" == "Darwin" ]]; then
+
+        while [[ $flag -eq 0 ]]; do
+            read -r -s -p "Enter password for new user: " PASS; printf '\n' >&2
+            if [[ ${#PASS} -lt 8 ]]; then 
+                read -p "Password too short. \nYou are sure you want to continue with a short password? [Y/n] " ANSWY; printf '\n' >&2
+                case "$ANSWY" in 
+                    Y|y|Yes|YES|'')
+                        ;;
+                    N|n|No|NO) 
+                        continue 
+                        ;;
+                    *)
+                        echo "Invalid input. Please enter Y/y or N/n."
+                        continue
+                        ;;
+                esac
+            fi
+            read -r -s -p "Enter password again: " PASS2; printf '\n' >&2
+            if [[ "$PASS" == "$PASS2" ]]; then
+                flag=1
+            else 
+                echo "Passwords do not match. Try again"
+            fi
+        done
+
+        /usr/sbin/sysadminctl -addUser "$NEW_USER" -fullName "$FULL_NAME" -home "$HOME_DIR" -shell "/bin/zsh" -password "$PASS" > /dev/null 2>&1
+        user_created=1
+        /usr/sbin/createhomedir -c -u "$NEW_USER" > /dev/null 2>&1
+        home_created=1
+        
+        chown "$NEW_USER:$(id -gn "$NEW_USER")" "$HOME_DIR" > /dev/null 2>&1
+        chmod 700 "$HOME_DIR" > /dev/null 2>&1
+
+        unset -v PASS PASS2
+
+        dseditgroup -o edit -a "$NEW_USER" -t user admin > /dev/null 2>&1
+        echo "User "$NEW_USER" was added to the admin group"
+
+        if id "$NEW_USER" &>/dev/null; then
+            echo "New user '$NEW_USER' at '$HOME_DIR' were created"
+            return 0
+        else
+            echo "Something went wrong"
+            exit 1
         fi
-        read -r -s -p "Enter password again: " PASS2; printf '\n' >&2
-        if [[ "$PASS" == "$PASS2" ]]; then
-            flag=1
-        else 
-            echo "Passwords do not match. Try again"
-        fi
-    done
-    useradd -m -c "$FULL_NAME" -s /bin/bash "$NEW_USER" > /dev/null 2>&1
-    user_created=1
-
-    HASH=$(openssl passwd -6 "$PASS")
-    usermod --password "$HASH" "$NEW_USER" > /dev/null 2>&1
-    pass_created=1
-
-    unset -v PASS PASS2 HASH
-
-    chown "$NEW_USER:$NEW_USER" "/home/$NEW_USER" > /dev/null 2>&1
-    chmod 700 "/home/$NEW_USER" > /dev/null 2>&1
-
-    usermod -aG sudo "$NEW_USER" > /dev/null 2>&1
-    echo "User '$NEW_USER' was added to the admin group"
-
-    if id "$NEW_USER" &>/dev/null; then
-        echo "New user '$NEW_USER' at '$HOME_DIR' were created"
-        return 0
     else 
-        echo "Something went wrong"
-        exit 1
-    fi
-}
+        local flag=0
+        while [[ $flag -eq 0 ]]; do
+            read -r -s -p "Enter password for new user: " PASS; printf '\n' >&2
+            if [[ ${#PASS} -lt 8 ]]; then
+                echo "Password too short"
+                read -e -p "Are you sure you want to continue with a short password? [Y/n]: " ANSWY; printf '\n' >&2
+                case "$ANSWY" in 
+                    Y|y|Yes|YES|'') ;;
+                    N|n|No|NO) continue ;;
+                    *) echo "Invalid input. Please enter Y/y or N/n"; continue ;;
+                esac
+            fi
+            read -r -s -p "Enter password again: " PASS2; printf '\n' >&2
+            if [[ "$PASS" == "$PASS2" ]]; then
+                flag=1
+            else 
+                echo "Passwords do not match. Try again"
+            fi
+        done
+        useradd -m -c "$FULL_NAME" -s /bin/bash "$NEW_USER" > /dev/null 2>&1
+        user_created=1
 
-# ---------- create_macos_user ----------
+        HASH=$(openssl passwd -6 "$PASS")
+        usermod --password "$HASH" "$NEW_USER" > /dev/null 2>&1
+        pass_created=1
 
-create_macos_user() {
-    local FULL_NAME PASS PASS2
-    read -r -p "Enter full name: " FULL_NAME
-    
-    local flag=0
-    while [[ $flag -eq 0 ]]; do
-        read -r -s -p "Enter password for new user: " PASS; printf '\n' >&2
-        if [[ ${#PASS} -lt 8 ]]; then 
-            read -p "Password too short. \nYou are sure you want to continue with a short password? [Y/n] " ANSWY; printf '\n' >&2
-            case "$ANSWY" in 
-                Y|y|Yes|YES|'')
-                    ;;
-                N|n|No|NO) 
-                    continue 
-                    ;;
-                *)
-                    echo "Invalid input. Please enter Y/y or N/n."
-                    continue
-                    ;;
-            esac
-        fi
-        read -r -s -p "Enter password again: " PASS2; printf '\n' >&2
-        if [[ "$PASS" == "$PASS2" ]]; then
-            flag=1
+        unset -v PASS PASS2 HASH
+
+        chown "$NEW_USER:$NEW_USER" "/home/$NEW_USER" > /dev/null 2>&1
+        chmod 700 "/home/$NEW_USER" > /dev/null 2>&1
+
+        usermod -aG sudo "$NEW_USER" > /dev/null 2>&1
+        echo "User '$NEW_USER' was added to the admin group"
+
+        if id "$NEW_USER" &>/dev/null; then
+            echo "New user '$NEW_USER' at '$HOME_DIR' were created"
+            return 0
         else 
-            echo "Passwords do not match. Try again"
+            echo "Something went wrong"
+            exit 1
         fi
-    done
-
-    /usr/sbin/sysadminctl -addUser "$NEW_USER" -fullName "$FULL_NAME" -home "$HOME_DIR" -shell "/bin/zsh" -password "$PASS" > /dev/null 2>&1
-    user_created=1
-    /usr/sbin/createhomedir -c -u "$NEW_USER" > /dev/null 2>&1
-    home_created=1
-    
-    chown "$NEW_USER:$(id -gn "$NEW_USER")" "$HOME_DIR" > /dev/null 2>&1
-    chmod 700 "$HOME_DIR" > /dev/null 2>&1
-
-    unset -v PASS PASS2
-
-    dseditgroup -o edit -a "$NEW_USER" -t user admin > /dev/null 2>&1
-    echo "User "$NEW_USER" was added to the admin group"
-
-    if id "$NEW_USER" &>/dev/null; then
-        echo "New user '$NEW_USER' at '$HOME_DIR' were created"
-        return 0
-    else
-        echo "Something went wrong"
-        exit 1
     fi
-    
 }
 
 # ---------- change_pass ----------
@@ -513,11 +508,7 @@ fi
 
 if [[ $DO_CREATE -eq 1 ]]; then
     start
-    if [[ "$OS" == "Darwin" ]]; then
-        create_macos_user
-    else
-        create_linux_user
-    fi
+    create_user
 elif [[ $DO_DELETE -eq 1 ]]; then
     read -p "Are you really sure about deleting user '$TARGET_USER'? [Y/n] " ANS; printf '\n'>&2
     case "$ANS" in 
